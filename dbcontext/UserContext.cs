25248eccc9def9613f37882b1381ea64334a9301
Quynh Nguyen <llaugusty@gmail.com>
2019-08-24 14:50:41 +0700

    Add user context :

--- dbcontext/UserContext.cs
+++ dbcontext/UserContext.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RCB.TypeScript.Models;
+
 namespace RCB.TypeScript.dbcontext
 {
-    public class UserContext
+    public class UserContext : DbContext
     {
-        public UserContext()
-        {
-        }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
