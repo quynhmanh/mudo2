e3b70e5197f8baebe9d05605a53a3e3f5e385bed
Quynh Nguyen <llaugusty@gmail.com>
2019-08-24 16:48:25 +0700

    update home page

--- ClientApp/pages/HomePage.tsx
+++ ClientApp/pages/HomePage.tsx
@@ -82,7 +82,7 @@ export default class HomePage extends React.Component<IProps, IState> {
               marginBottom: '20px',
               fontFamily: 'AvenirNextRoundedPro-Bold',
             }}
-          >Thiết kế mọi thứ 5</h2>
+          >Thiết kế mọi thứ 6</h2>
           <div
             id="search-icon"
             style={{
