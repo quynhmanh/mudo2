9ad5c8c9bec7dbebe41418749b1e8c1db7bcf399
Quynh Nguyen <llaugusty@gmail.com>
2019-08-26 21:49:46 +0700

    Update design

--- Controllers/DesignController.cs
+++ Controllers/DesignController.cs
@@ -103,11 +103,18 @@ namespace RCB.TypeScript.Controllers
                     for (var i = 0; i < canvas.Length; ++i)
                     {
                         var html = template.Replace("[CANVAS]", canvas[i]);
-                        byte[] bytes = Encoding.ASCII.GetBytes(html);
-                        using (var htmlFile = new FileStream("/Users/llaugusty/Downloads/quynh2.html", FileMode.Create))
+                        try
                         {
-                            htmlFile.Write(bytes, 0, bytes.Length);
-                            htmlFile.Flush();
+                            byte[] bytes = Encoding.ASCII.GetBytes(html);
+                            using (var htmlFile = new FileStream("/Users/llaugusty/Downloads/quynh2.html", FileMode.Create))
+                            {
+                                htmlFile.Write(bytes, 0, bytes.Length);
+                                htmlFile.Flush();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+
                         }
 
                         await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
