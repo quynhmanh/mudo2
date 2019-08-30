a140ea1da564871d874de8f1573ea263e5934f42
Quynh Nguyen <llaugusty@gmail.com>
2019-08-30 13:35:18 +0700

    update base model

--- Models/Base.cs
+++ Models/Base.cs
@@ -6,12 +6,12 @@ namespace RCB.TypeScript.Models
     public class Base
     {
         [DataType(DataType.Date)]
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
 
         public int CreatedBy { get; set; }
 
         [DataType(DataType.Date)]
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
         public int UpdatedBy { get; set; }
     }
 }
