0dd7a6be200d0c8abcb2c45d75eac20f36281815
Quynh Nguyen <llaugusty@gmail.com>
2019-09-06 11:14:43 +0700

    update components

--- ClientApp/components/shared/ImagePicker.tsx
+++ ClientApp/components/shared/ImagePicker.tsx
@@ -23,9 +23,9 @@ const Container = styled.div`
   background-size: 300% 300%;
   position: relative;
   width: ${props => props.width}px;
-  animation: gradient___UgeAv 4s linear infinite;
+  animation: gradient___UgeAv 3s linear infinite;
   height: ${props => props.loaded ? props.height : props.defaultHeight + "px"};
-  background-image: ${props => props.loaded ? 'none' : 'linear-gradient(90deg,#019fb6,#019fb6,#019FBD,#76c5d0,#2ea6c5,#019FB1,#019fb6'} );
+  background-image: ${props => props.loaded ? 'none;' : 'linear-gradient(90deg,#efefef00,rgba(255, 255, 255, .15),#ffffff26,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.9),#060c0d00 );'}
   margin-bottom: 10px;
   button {
     visibility: hidden;
