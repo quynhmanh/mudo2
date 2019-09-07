7ad02a8f7a3ae9a7366c63dfcaaacaf83aeceaa0
Quynh Nguyen <llaugusty@gmail.com>
2019-09-08 06:09:28 +0700

    Update controller

--- Controllers/DesignController.cs
+++ Controllers/DesignController.cs
@@ -428,23 +428,28 @@ namespace RCB.TypeScript.Controllers
 
                         var path = "/Users/llaugusty/Downloads/puppeteer-tab-capture-repro/test-extension";
                         var extensionId = "ihfahmlcdcnbdmbjlohjpgbiknhljmdc";
-                        var args = new string[] {
-            $"--whitelisted-extension-id={extensionId}",
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            $"--disable-extensions-except={path}",
-            $"--load-extension={path}"
+            //            var args = new string[] {
+            //$"--whitelisted-extension-id={extensionId}",
+            //"--no-sandbox",
+            //"--disable-setuid-sandbox",
+            //"--disable-dev-shm-usage",
+            //$"--disable-extensions-except={path}",
+            //$"--load-extension={path}"
+            //            };
+
+                        var args = new string[]
+                        {
+                            "--no-sandbox",
                         };
 
                         await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
                         var browser = await Puppeteer.LaunchAsync(new LaunchOptions
                         {
-                            //DefaultViewport = new ViewPortOptions()
-                            //{
-                            //    Width = (int)double.Parse(width),
-                            //    Height = (int)double.Parse(height),
-                            //},
+                            DefaultViewport = new ViewPortOptions()
+                            {
+                                Width = (int)double.Parse(width),
+                                Height = (int)double.Parse(height),
+                            },
                             ExecutablePath = "/usr/bin/chromium-browser",
                             Args = args,
                             Headless = false,
