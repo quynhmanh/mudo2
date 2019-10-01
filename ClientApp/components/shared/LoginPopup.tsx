cccf07f16e2667044c1849a893ef85a5febcf5e3
Quynh Nguyen <llaugusty@gmail.com>
2019-10-01 16:45:01 +0700

    update login style

--- ClientApp/components/shared/LoginPopup.tsx
+++ ClientApp/components/shared/LoginPopup.tsx
@@ -134,7 +134,7 @@ class LoginPopup extends PureComponent<IProps, IState> {
                      background: 'white',
                   }} >
                     <div style={{
-                      width: '400px',
+                      width: '550px',
                       backgroundColor: '#638efc',
                       padding: '40px',
                     }}> 
@@ -173,7 +173,7 @@ class LoginPopup extends PureComponent<IProps, IState> {
                             textDecoration: 'underline',
                           }} className="logInBtn___HgB1o" data-test="changeModal" data-categ="registrationModal" data-value="goToLoginModal">Miễn phí tạo ngay</button></div></div>
                     </div>
-                    <div style={{position: 'relative', width: '100%',}}>
+                    <div style={{position: 'relative',}}>
                     {!this.props.externalProviderCompleted ?   
                     <div id="loginContainer" 
             style={{
