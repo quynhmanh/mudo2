2dff0b4714c85823e406dd3577bc56c82e067035
Quynh Nguyen <llaugusty@gmail.com>
2019-10-03 14:12:22 +0700

    fix deploy

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+Xvfb -ac :99 -screen 0 1280x1024x16 &
+
+dotnet RCB.TypeScript.dll
+
 wget http://download.tensorflow.org/models/deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz
 wget http://download.tensorflow.org/models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz
 
@@ -8,7 +12,3 @@ tar xvzf deeplabv3_pascal_train_aug_2018_01_04.tar.gz -C xception_model --strip=
 
 rm deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz
 rm deeplabv3_pascal_train_aug_2018_01_04.tar.gz
-
-Xvfb -ac :99 -screen 0 1280x1024x16 &
-
-dotnet RCB.TypeScript.dll
