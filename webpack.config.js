7f2dc2b90f8d68e9b82097e9f85bd392658ef651
Quynh Nguyen <llaugusty@gmail.com>
2019-10-12 20:40:13 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -101,7 +101,8 @@ module.exports = (env) => {
                 // solution that requires the user to opt into importing specific locales.
                 // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
                 // You can remove this if you don't use Moment.js:
-                new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+                new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+                new LoadablePlugin()
             ].concat(isDevBuild ? [
                 // Development.
 
@@ -169,7 +170,8 @@ module.exports = (env) => {
             new webpack.DllReferencePlugin({
                 context: __dirname,
                 manifest: require('./wwwroot/dist/vendor-manifest.json')
-            })
+            }),
+            new LoadablePlugin()
         ].concat(isDevBuild ? [
             // Development.
             new webpack.SourceMapDevToolPlugin({
