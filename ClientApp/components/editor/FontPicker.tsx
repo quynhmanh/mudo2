c96efc01bd45b385687244e9e02b1790a868fef0
Quynh Nguyen <llaugusty@gmail.com>
2020-09-25 20:06:20 +0700

    update

--- ClientApp/components/editor/FontPicker.tsx
+++ ClientApp/components/editor/FontPicker.tsx
@@ -35,7 +35,7 @@ export default class FontPicker extends Component<IProps, IState> {
                 style={{
                     height: this.props.height + "px",
                     backgroundColor: this.state.loaded ? 'transparent' : '#80808026',
-                    width: "200px",
+                    // width: "200px",
                 }}>
                 <img
                     onLoad={(e) => {
@@ -44,7 +44,6 @@ export default class FontPicker extends Component<IProps, IState> {
                     }
                     style={{
                         height: this.props.height + "px",
-                        width: "200px",
                         display: this.state.loaded ? 'block' : 'none',
                     }}
                     src={this.props.src}
