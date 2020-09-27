e70f12f7141ac75757a85c8b82b0236cd2219708
Quynh Nguyen <llaugusty@gmail.com>
2020-09-27 23:56:40 +0700

    update

--- ClientApp/components/editor/SidebarElement.tsx
+++ ClientApp/components/editor/SidebarElement.tsx
@@ -452,6 +452,15 @@ export default class SidebarEffect extends Component<IProps, IState> {
                     }}
                     // value={this.state.query}
                 />
+                <button 
+                    onClick={e => this.handleQuery("")}
+                    style={{
+                        position: 'absolute',
+                        right: '15px',
+                        top: '15px',
+                        border: 'none',
+                    }}
+                type="button"><span class="TcNIhA"><span aria-hidden="true" class="NA_Img dkWypw"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="black" d="M13.06 12.15l5.02-5.03a.75.75 0 1 0-1.06-1.06L12 11.1 6.62 5.7a.75.75 0 1 0-1.06 1.06l5.38 5.38-5.23 5.23a.75.75 0 1 0 1.06 1.06L12 13.2l4.88 4.87a.75.75 0 1 0 1.06-1.06l-4.88-4.87z"></path></svg></span></span></button>
                 <div style={{ display: "inline-block", width: "100%", height: "calc(100% - 65px)" }}>
                 {!this.state.query && <div>
                     <SidebarElement 
