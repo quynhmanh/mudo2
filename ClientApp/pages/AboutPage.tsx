27157a28842c7933145d92d2e7ef0a57ef199d9b
Quynh Nguyen <llaugusty@gmail.com>
2020-09-29 20:57:48 +0700

    update

--- ClientApp/pages/AboutPage.tsx
+++ ClientApp/pages/AboutPage.tsx
@@ -46,20 +46,8 @@ export default class PrintPage extends React.Component<IProps, IState> {
                                             <div className="">
                                                 <div className="col-sm mb-16">
                                                     <div className="card center-card">
-                                                        <div className="card-body">
-                                                            <img
-                                                                style={{
-                                                                    height: '150px',
-                                                                    borderRadius: '50%',
-                                                                }}
-                                                                src="https://avatars2.githubusercontent.com/u/9525970?s=400&u=729b463051f86ba79d9aa05c3c19b3420b162be4&v=4" className="card-img-right avatar-img" />
-                                                            <h5 className="card-title">Quynh Nguyen</h5>
-                                                            <p className="card-text text-muted">Founder of Draft.vn</p>
-                                                            <a target="_blank" href="https://twitter.com/_nghiatran"><i className="fa fa-twitter mr-8" /></a>
-                                                            <a target="_blank" href="https://github.com/NghiaTranUIT"><i className="fa fa-github mr-8" /></a>
-                                                            <a target="_blank" href="https://www.linkedin.com/in/vinhnghiatran"><i className="fa fa-linkedin mr-8" /></a>
-                                                            <a target="_blank" href="https://www.facebook.com/nghia.tran.9883"><i className="fa fa-facebook mr-8" /></a>
-                                                        </div>
+                                                        
+      <div className="card-body" style={{display: 'inline-block', border: '1px solid rgba(0,0,0,.125)', width: '300px', height: '150px', padding: '0 20px', position: 'relative', borderRadius: '.25rem', right: '24px'}}><img src="https://avatars2.githubusercontent.com/u/9525970?s=400&u=729b463051f86ba79d9aa05c3c19b3420b162be4&v=4" className="card-img-right avatar-img" style={{height: '60px', borderRadius: '50%', position: 'absolute', right: '10px', top: '10px'}} /><h3 className="card-title">Quynh Nguyen</h3><p className="card-text text-muted">Founder of Draft.vn</p><a target="_blank" href="https://twitter.com/_nghiatran"><i className="fa fa-twitter mr-8" /></a><a target="_blank" href="https://github.com/NghiaTranUIT"><i className="fa fa-github mr-8" /></a><a target="_blank" href="https://www.linkedin.com/in/vinhnghiatran"><i className="fa fa-linkedin mr-8" /></a><a target="_blank" href="https://www.facebook.com/nghia.tran.9883"><i className="fa fa-facebook mr-8" /></a></div>
                                                     </div>
                                                 </div>
                                             </div>
