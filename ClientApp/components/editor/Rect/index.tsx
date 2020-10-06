903ebf7013f21bd9e8c0bb54fae3ce2e67f80293
Quynh Nguyen <llaugusty@gmail.com>
2020-10-06 11:36:20 +0700

    update

--- ClientApp/components/editor/Rect/index.tsx
+++ ClientApp/components/editor/Rect/index.tsx
@@ -131,7 +131,7 @@ export default class Rect extends Component<IProps, IState> {
 						if (ell.tagName == "stop") {
 							if (!field) field = "stopColor";
 							ell.style[field] = this.props.image.colors[j];
-						} else if (ell.tagName == "path" || ell.tagName == "circle" || ell.tagName == "g") {
+						} else if (ell.tagName == "path" || ell.tagName == "circle" || ell.tagName == "g" || ell.tagName == "polygon") {
 							if (!field) field = "fill";
 							ell.style[field] = this.props.image.colors[j];
 						}
