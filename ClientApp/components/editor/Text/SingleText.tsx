0515163c3d050191b1969a35114938b19c9f63e3
Quynh Nguyen <llaugusty@gmail.com>
2020-11-03 09:19:03 +0700

    update

--- ClientApp/components/editor/Text/SingleText.tsx
+++ ClientApp/components/editor/Text/SingleText.tsx
@@ -151,7 +151,6 @@ export default class SingleText extends PureComponent<IProps, IState> {
                         fontStyle: italic ? "italic" : "",
                         fontWeight: bold ? "bold" : "normal",
                         letterSpacing: getLetterSpacing(letterSpacing),
-                        overflow: "hidden",
                     }}
                 >
                     <div
