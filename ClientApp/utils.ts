9a21fd0ad8c4edbc36548b5c54dc5b34e7f1ac6f
Quynh Nguyen <quynhnguyen@Quynhs-MacBook-Pro.local>
2020-11-03 17:37:22 +0700

    update

--- ClientApp/utils.ts
+++ ClientApp/utils.ts
@@ -949,7 +949,7 @@ export const handleBlockAnimation = (injectScriptOnly = false) => {
                             newNode.id = image._id + child._id + "animation-block";
                             newNode.style.background = child.color;
                             newNode.style.opacity = 1;
-                            newNode.style.left = "-" + (newNode.style.width + 1);
+                            newNode.style.left = "-" + (child.width + 10) + "px";
                         }
                     }
                 });
