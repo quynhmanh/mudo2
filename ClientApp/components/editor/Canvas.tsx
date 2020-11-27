2dc8d9cff6460a14683f7e400f36441a78551ff3
Quynh Nguyen <llaugusty@gmail.com>
2020-11-27 19:46:53 +0700

    update

--- ClientApp/components/editor/Canvas.tsx
+++ ClientApp/components/editor/Canvas.tsx
@@ -161,7 +161,7 @@ export default class Canvas extends Component<IProps, IState> {
                             <Tooltip
                                 offsetLeft={0}
                                 offsetTop={-15}
-                                content={"Delete page"}
+                                content={window.translate("deletePage")}
                                 position="top"
                             >
                                 <button
