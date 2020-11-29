8d30565e4d95f14927210816803d5238947f88e9
Quynh Nguyen <llaugusty@gmail.com>
2020-11-29 09:54:02 +0700

    update

--- ClientApp/components/editor/FontSize.tsx
+++ ClientApp/components/editor/FontSize.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import ResizableRect from '@Components/editor/ResizableRect';
 import uuidv4 from "uuid/v4";
 import Tooltip from '@Components/shared/Tooltip';
-import {htmlToImage, getBoundingClientRect, handleFontSizeBtnClick} from '@Utils';
+import { htmlToImage, getBoundingClientRect, handleFontSizeBtnClick } from '@Utils';
 import "@Styles/editor.scss";
 import TopMenu from '@Components/editor/Sidebar';
 import axios from 'axios';
@@ -27,81 +27,81 @@ enum Mode {
     CreateTextTemplate = 2,
     EditTemplate = 3,
     EditTextTemplate = 4,
-  }
+}
 
 export default class FontSize extends Component<IProps, IState> {
 
-  constructor(props: any) {
-    super(props);
-  }
-
-  shouldComponentUpdate = (nextProps, nextState) => {
-      if (this.props.fontSize != nextProps.fontSize) {
-      (document.getElementById("fontSizeButton") as HTMLInputElement).value = Math.round(nextProps.fontSize).toString();
+    constructor(props: any) {
+        super(props);
     }
-    return true;
-  }
+
+    shouldComponentUpdate = (nextProps, nextState) => {
+        if (this.props.fontSize != nextProps.fontSize) {
+            (document.getElementById("fontSizeButton") as HTMLInputElement).value = Math.round(nextProps.fontSize).toString();
+        }
+        return true;
+    }
 
     onClickDropDownFontSizeList = () => {
         document.getElementById("myFontSizeList").classList.toggle("show");
         document.getElementById("fontSizeButton").style.backgroundColor = "#f2f5f7";
 
         const onDown = e => {
-          if (!e.target.matches(".dropbtn-font-size")) {
-            document.getElementById("fontSizeButton").style.backgroundColor = "transparent";
-            var dropdowns = document.getElementsByClassName(
-              "dropdown-content-font-size"
-            );
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-              var openDropdown = dropdowns[i];
-              if (openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show");
-              }
-            }
-    
-            document.removeEventListener("mouseup", onDown);
-          }
-        };
-    
-        document.addEventListener("mouseup", onDown);
-      };
-
-  render () {
-    return (
-        <React.Fragment>
-          <img 
-            src={require("@Components/shared/svgs/editor/toolbar/arrowDown.svg")} 
-            alt={this.props.content} 
-            style={{ 
-              pointerEvents: 'none', 
-              position: 'absolute', 
-              top: 0,
-              bottom: 0,
-              right: "4px",
-              margin: "auto",
-            }}/> 
-          <input
-              id="fontSizeButton"
-              className="dropbtn-font dropbtn-font-size"
-              defaultValue={Math.round(this.props.fontSize).toString()}
-              onClick={ e=> {
-                document.execCommand('selectall', null);
-                this.onClickDropDownFontSizeList.bind(this)(e);
-              }}
-              onKeyDown={e => {
-                e.nativeEvent.stopImmediatePropagation();
-                if (e.keyCode == 13) {
-                    var val = (e.target as HTMLInputElement).value;
-                    handleFontSizeBtnClick(e, val);
-                    window.getSelection().removeAllRanges();
-                    document.getElementById("myFontSizeList").classList.toggle("show");
+            if (!e.target.matches(".dropbtn-font-size")) {
+                document.getElementById("fontSizeButton").style.backgroundColor = "transparent";
+                var dropdowns = document.getElementsByClassName(
+                    "dropdown-content-font-size"
+                );
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains("show")) {
+                        openDropdown.classList.remove("show");
+                    }
                 }
-            }}
-          >
-            {/* {Math.round(this.props.fontSize)} */}
-          </input>
-        </React.Fragment>
-    );
-  }
+
+                document.removeEventListener("mouseup", onDown);
+            }
+        };
+
+        document.addEventListener("mouseup", onDown);
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <img
+                    src={require("@Components/shared/svgs/editor/toolbar/arrowDown.svg")}
+                    alt={this.props.content}
+                    style={{
+                        pointerEvents: 'none',
+                        position: 'absolute',
+                        top: 0,
+                        bottom: 0,
+                        right: "4px",
+                        margin: "auto",
+                    }} />
+                <input
+                    id="fontSizeButton"
+                    className="dropbtn-font dropbtn-font-size"
+                    defaultValue={Math.round(this.props.fontSize).toString()}
+                    onClick={e => {
+                        document.execCommand('selectall', null);
+                        this.onClickDropDownFontSizeList.bind(this)(e);
+                    }}
+                    onKeyDown={e => {
+                        e.nativeEvent.stopImmediatePropagation();
+                        if (e.keyCode == 13) {
+                            var val = (e.target as HTMLInputElement).value;
+                            handleFontSizeBtnClick(e, val);
+                            window.getSelection().removeAllRanges();
+                            document.getElementById("myFontSizeList").classList.toggle("show");
+                        }
+                    }}
+                >
+                    {/* {Math.round(this.props.fontSize)} */}
+                </input>
+            </React.Fragment>
+        );
+    }
 }
--- ClientApp/components/editor/HomeButton.tsx
+++ ClientApp/components/editor/HomeButton.tsx
@@ -64,9 +64,100 @@ export default class HomeButton extends React.Component<IProps, IState> {
                             fontWeight: 500,
                         }}
                     >{this.props.translate("home")}</span>
-                    
                 </div>
             </a>
+            <div
+                id="logo-editor"
+                style={{
+                    color: "white",
+                    display: "flex",
+                    padding: "5px 14px",
+                    position: "relative",
+                    borderRadius: "3px",
+                    marginLeft: "5px",
+                    textDecoration: "none",
+                }}
+            >
+                <a
+                    style={{
+                        color: "white",
+                        textDecoration: "none",
+                    }}
+                    href="javascript:"
+                    onClick={e => {
+                        document.getElementById("myResizeList").classList.toggle("show");
+                
+                        const onDown = e => {
+                            if (!document.getElementById("myResizeList").contains(e.target)) {
+                                // if (!e.target.matches(".dropbtn-font-size")) {
+                                let dropdowns = document.getElementsByClassName(
+                                    "dropdown-content-font-size"
+                                );
+                                let i;
+                                for (i = 0; i < dropdowns.length; i++) {
+                                    let openDropdown = dropdowns[i];
+                                    if (openDropdown.classList.contains("show")) {
+                                        openDropdown.classList.remove("show");
+                                    }
+                                }
+                    
+                                document.removeEventListener("mouseup", onDown);
+                            }
+                        };
+                
+                        document.addEventListener("mouseup", onDown);
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            fontSize: "15px",
+                            fontWeight: 600,
+                            height: "100%",
+                            position: "relative",
+                        }}
+                    >
+                        Resize
+                    </div>
+                </a>
+            <div
+                id="myResizeList"
+                style={{
+                    left: "5px",
+                    zIndex: 9999999999,
+                    background: "white",
+                    display: "none" ,
+                    color: "black",
+                    flexDirection: "column",
+                    padding: "20px",
+                }}
+                className="dropdown-content-font-size dropbtn-font-size"
+            >
+                <form style={{margin: 0,}} action="/action_page.php">
+                    <input style={{margin: "15px 10px",}} type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+                    <span>Menu</span>
+                    <br/>
+                    <input style={{margin: "15px 10px",}} type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
+                    <span>Instagram Post</span>
+                    <br/>
+                    <input style={{margin: "15px 10px",}} type="checkbox" id="vehicle3" name="vehicle3" value="Boat"/>
+                    <span>Facebook Post</span>
+                    <br/>
+                </form>
+                <button
+                    style={{
+                        width: "100px",
+                        border: "none",
+                        background: "#80808030",
+                        padding: "7px 12px",
+                        borderRadius: "5px",
+                        marginTop: "10px",
+                        marginLeft: "9px",
+                    }}
+                >Resize</button>
+            </div>
+            </div>
             <p
                 id="savingState"
                 style={{
